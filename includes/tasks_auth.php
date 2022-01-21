2f092183c8f68fe372e9675e4d1d8bdd4855418d
farhan-fm <farhanfakhrzadeh1999@gmail.com>
2022-01-21 23:11:50 +0330

    some change

--- includes/tasks_auth.php
+++ includes/tasks_auth.php
@@ -1,42 +1,47 @@
 <?php
 
-if(isset($_POST['submit-task'])) {
-        $task = $_POST['task'];
+if (isset($_POST['submit-task'])) {
+    $task = $_POST['task'];
+    $task_status = $_POST['status'];
+    $status = substr($task_status, 1, -1);
+    $status = substr($status,2);
+    $status = substr_replace($status ,"",-2);
 
-        $query = "INSERT INTO tasks (user_id, task) VALUES (" . $_SESSION['user_id'] . ", '$task')";
-        var_dump($query);
-        $result = mysqli_query($conn, $query);
 
-        if($result) {
-            echo $_SESSION['task-message'] = "New task added";
-            header("Location: tasks.php");
-            exit(0);
-        } else {
-            die("Nothing to show".mysqli_error($conn));
-        }
+    $query = "INSERT INTO tasks (user_id, task, status) VALUES (" . $_SESSION['user_id'] . ", '$task' , '$status' )";
+    var_dump($query);
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
+        echo $_SESSION['task-message'] = "New task added";
+        header("Location: tasks.php");
+        exit(0);
+    } else {
+        die("Nothing to show" . mysqli_error($conn));
+    }
+}
+
+if (isset($_POST['logout'])) {
+    header("Location: logout.php");
+}
+
+if (isset($_GET['edit_id'])) {
+    header("Location: edit.php");
+}
+
+if (isset($_GET['delete_id'])) {
+    $task_id = $_GET['delete_id'];
+
+    $query = "DELETE FROM tasks WHERE task_id=$task_id";
+    $delete_result = mysqli_query($conn, $query);
+
+
+    if (!$delete_result) {
+        die("Nothing!" . mysqli_error($conn));
+    } else {
+        $_SESSION['delete'] = "Task Deleted";
+        header("Location: tasks.php");
+        exit(0);
     }
 
-    if(isset($_POST['logout'])) {
-        header("Location: logout.php");
-    }
-
-    if(isset($_GET['edit_id'])) {
-        header("Location: edit.php");
-    }
-
-    if(isset($_GET['delete_id'])) {
-        $task_id = $_GET['delete_id'];
-        
-        $query = "DELETE FROM tasks WHERE task_id=$task_id";
-        $delete_result = mysqli_query($conn, $query);
-        
-        
-        if(!$delete_result) {
-            die("Nothing!".mysqli_error($conn));
-        } else {   
-            $_SESSION['delete'] = "Task Deleted";
-            header("Location: tasks.php");
-            exit(0);
-        }
-        
-    }
+}
--- tasks.php
+++ tasks.php
@@ -28,45 +28,47 @@ include "includes/tasks_auth.php";
 
 <form action="tasks.php" method="post" style="width: 20% ; height: 100%">
     <div class="card-body" style="float: left">
-        <h3 style="direction: rtl ; float-displace: block ; position: relative ; alignment: center">سلام <?php echo $_SESSION['username'] ?>خوش آمدید</h3>
+        <h3 style="direction: rtl ; float-displace: block ; position: relative ; alignment: center">
+            سلام <?php echo $_SESSION['username'] ?>خوش آمدید</h3>
         <input class="logout" type="submit" name="logout" value="خروج" style="margin: 0 20px ; position: relative">
+        <div class="flash-message">
+            <?php
+            if (isset($_SESSION['success'])) {
+                echo "<h4 class='flash-message'>" . $_SESSION['success'] . "</h4>";
+                unset($_SESSION['success']);
+            }
+            ?>
+            <h4>
+                <?php
+                if (isset($_SESSION['task-message'])) {
+                    echo $_SESSION['task-message'];
+                    unset($_SESSION['task-message']);
+                }
+                ?>
+            </h4>
+            <h4>
+                <?php
+                if (isset($_SESSION['delete'])) {
+                    echo $_SESSION['delete'];
+                    unset($_SESSION['delete']);
+                }
+                ?>
+            </h4>
+            <h4>
+                <?php
+                if (isset($_SESSION['edit'])) {
+                    echo $_SESSION['edit'];
+                    unset($_SESSION['edit']);
+                }
+                ?>
+            </h4>
+        </div>
+
     </div>
 
 
 </form>
 
-<div class="flash-message">
-    <?php
-    if (isset($_SESSION['success'])) {
-        echo "<h4 class='flash-message'>" . $_SESSION['success'] . "</h4>";
-        unset($_SESSION['success']);
-    }
-    ?>
-    <h4>
-        <?php
-        if (isset($_SESSION['task-message'])) {
-            echo $_SESSION['task-message'];
-            unset($_SESSION['task-message']);
-        }
-        ?>
-    </h4>
-    <h4>
-        <?php
-        if (isset($_SESSION['delete'])) {
-            echo $_SESSION['delete'];
-            unset($_SESSION['delete']);
-        }
-        ?>
-    </h4>
-    <h4>
-        <?php
-        if (isset($_SESSION['edit'])) {
-            echo $_SESSION['edit'];
-            unset($_SESSION['edit']);
-        }
-        ?>
-    </h4>
-</div>
 
 <div class="task-header" style="direction: rtl; padding-top: 16px ; padding-bottom: 16px">
     <h3>نرم افزار لیست todo با استفاده از php</h3>
@@ -76,21 +78,52 @@ include "includes/tasks_auth.php";
     <div class="input-group">
         <input class="task-input" type="text" name="task" style="direction: rtl" placeholder="تسک را وارد کنید">
     </div>
+
+
+    <select name="status" type="text" style="float: left">
+        <option value=”todo”>ToDo</option>
+        <option value=”doing”>Doing</option>
+        <option value=”done”>Done</option>
+    </select>
+
     <br/>
-    <input type="submit" style="margin: 0 45%; position: relative" class="btn btn-primary" name="submit-task" value="ذخیره">
+    <input type="submit" style="margin: 0 45%; position: relative" class="btn btn-primary" name="submit-task"
+           value="ذخیره">
+
 </form>
+
+<label for="filter" style="float: left ; direction: ltr ; margin-left: 8px"> filter : </label>
+<button onclick="filter1()" type="button" value="all">همه</button>
+<button onclick="filter2()" type="button" value="todo">todo</button>
+<button onclick="filter3()" type="button" value="doing">doing</button>
+<button onclick="filter4()" type="button" value="done">done</button>
 <table>
     <thead>
     <tr>
         <th>شماره تسک</th>
         <th>نام کاربر</th>
         <th>تسک</th>
+        <th>وضعیت</th>
         <th>عملیات</th>
     </tr>
     </thead>
     <tbody>
     <?php
-    $s_query = "SELECT * FROM tasks WHERE user_id=" . $_SESSION['user_id'];
+
+    $filter = 1;
+
+    $s_query = '';
+    echo "this is :" . $filter;
+    if ($filter == 4) {
+        $s_query = "SELECT * FROM tasks WHERE user_id=" . $_SESSION['user_id'] . " AND status =" . '"done"';
+    } else {
+        $s_query = "SELECT * FROM tasks WHERE user_id=" . $_SESSION['user_id'];
+    }
+    //else if ($filter1 == 1) {
+    //    $s_query = "SELECT * FROM tasks WHERE user_id=" . $_SESSION['user_id'];
+    // }
+
+
     $s_result = mysqli_query($conn, $s_query);
 
     $i = 0;
@@ -99,7 +132,7 @@ include "includes/tasks_auth.php";
         $task_id = $row['task_id'];
         $user_id = $row['user_id'];
         $task = $row['task'];
-
+        $task_status = $row['status'];
 
         if ($i <= $task_id) {
             $i++;
@@ -109,6 +142,7 @@ include "includes/tasks_auth.php";
             <td><?php echo $i; ?></td>
             <td><?php echo $_SESSION['username']; ?></td>
             <td><?php echo $task; ?></td>
+            <td><?php echo $task_status; ?></td>
             <div>
                 <td>
                     <input type="checkbox">
@@ -124,4 +158,30 @@ include "includes/tasks_auth.php";
     </tbody>
 </table>
 </body>
+<script
+>
+    function filter1() {
+        <?php
+        $filter = 1;
+        ?>
+    }
+    function filter2() {
+        <?php
+        $filter = 2;
+        ?>
+    }
+
+    function filter3() {
+        <?php
+        $filter = 3;
+        ?>
+    }
+
+    function filter4() {
+        <?php
+        $filter = 4; ?>
+    }
+
+
+</script>
 </html>
